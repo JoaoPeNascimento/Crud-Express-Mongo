0de332f98f3c24410a37cb3ad090ec6951847c4c
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-05-23 11:08:27 -0300

    Adicionando validação com Zod

--- src/routes/user-routes/postUser.js
+++ src/routes/user-routes/postUser.js
@@ -1,7 +1,14 @@
 const UserModel = require("../../models/user.models");
+const { userSchema } = require("../../schemas/userSchema");
 
 module.exports = (router) => {
   router.post("/user", async (req, res) => {
+    const result = userSchema.safeParse(req.body);
+
+    if (!result.success) {
+      return res.status(400).send(result.error.issues);
+    }
+
     try {
       await UserModel.create(req.body);
 
