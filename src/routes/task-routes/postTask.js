c21dde33e4c4cadd9407c027b3e3e995a7ffa3a8
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-05-23 11:20:51 -0300

    Adicionando validação para Task

--- src/routes/task-routes/postTask.js
+++ src/routes/task-routes/postTask.js
@@ -1,7 +1,14 @@
 const TaskModel = require("../../models/task.model");
+const { taskSchema } = require("../../schemas/taskSchema");
 
 module.exports = (router) => {
   router.post("/task", async (req, res) => {
+    const result = taskSchema.safeParse(req.body);
+
+    if (!result.success) {
+      return res.status(400).send(result.error.errors);
+    }
+
     try {
       await TaskModel.create(req.body);
       res.status(200).send("Tarefa criada com sucesso!");
